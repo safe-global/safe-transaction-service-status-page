63fe06dfd4164de523149a2ae587b1724ed9530c
Yago Pérez Vázquez <yago@safe.global>
2025-11-06 14:02:33 +0100

    chore: Update Build folder (#216)

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 import { configDefaults } from "vitest/config";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+  },
   resolve: {
     alias: {
       src: path.resolve(__dirname, "./src"),
